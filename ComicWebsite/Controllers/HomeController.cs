fc382e8c77b8c7454fd3be0d4c0835b4ba93a9b3
Mteebash <20232212@ctucareer.co.za>
2023-08-11 11:56:45 +0200

    add YAML file

---
+++ ComicWebsite/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult LatestNews()
+        {
+            return Redirect("https://bleedingcool.com/tag/judge-dredd/");
+        }
+
+        public ActionResult Favourites()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Your Location page.";
+
+            return View();
+        }
+
+        
+        
+    }
+}
